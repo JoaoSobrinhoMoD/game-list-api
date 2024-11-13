23241f90b9d4007636e1f78ebd31892eccee7328
JoaoSobrinhoMoD <joaosobrinho9000@gmail.com>
2024-11-13 18:06:00 -0300

    GET game by id

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -12,26 +12,33 @@ public class GameMinDTO {
 
 	public GameMinDTO() {
 	}
-     //Não precisa mais utilizar o 'this" pois não tem mais ambiguidade um parâmetro com o mesmo nome.
-	public GameMinDTO(Game entity) { 
+
+	// Não precisa mais utilizar o 'this" pois não tem mais ambiguidade um parâmetro
+	// com o mesmo nome.
+	public GameMinDTO(Game entity) {
 		id = entity.getId();
 		title = entity.getTitle();
 		year = entity.getYear();
 		imgUrl = entity.getImgUrl();
 		shortDescription = entity.getShortDescription();
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public Integer getYear() {
 		return year;
 	}
+
 	public String getImgUrl() {
 		return imgUrl;
 	}
+
 	public String getShortDescription() {
 		return shortDescription;
 	}
